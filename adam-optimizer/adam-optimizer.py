6569c4f1b95873b1712aff386628ebb4af39c65e
Debojit Roy <139033817+night-spring@users.noreply.github.com>
2026-03-15 21:21:27 +0530

    Update solution for adam-optimizer

--- adam-optimizer/adam-optimizer.py
+++ adam-optimizer/adam-optimizer.py
@@ -12,11 +12,11 @@ def adam_step(param, grad, m, v, t, lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
     v = np.array(v)
     
     m_t = (beta1 * m) + ((1 - beta1) * grad)
-    v_t = (beta2 * v) + ((1 - beta2) * np.power(grad, 2))
+    v_t = (beta2 * v) + ((1 - beta2) * grad**2)
     
     m_new = m_t / (1 - beta1**t)
     v_new = v_t / (1 - beta2**t)
 
-    param_new = param - (lr * (m_new / (np.power(v_new, 0.5) + eps)))
+    param_new = param - (lr * (m_new / (np.sqrt(v_new) + eps)))
 
     return (param_new, m_t, v_t)
